d08d9a4413191b80ffdb573d114127f14d86d47c
Kamila <142943209+KamilaHaigert@users.noreply.github.com>
2024-02-26 23:17:29 -0300

    commit
    fazendo um commit

---
+++ Biblioteca_Sem_Fronteiras/generos.js
@@ -0,0 +1,3 @@
+document.getElementById("list-g").addEventListener('click',()=>{
+    document.getElementById('ul-generos').classList.toggle('display-none')
+})
---
+++ Biblioteca_Sem_Fronteiras/menu.js
@@ -0,0 +1,7 @@
+function abrirMenu() {
+    document.getElementById('menuLateral').classList.remove('display-none');
+}
+
+function fecharMenu() {
+    document.getElementById('menuLateral').classList.add('display-none');
+}
